68551448e2104558154e27646ad4985b7ffb5475
Dan Raskin <Danieltraskin@gmail.com>
2023-03-24 16:05:51 -0500

    basic styling

---
+++ components/Button.tsx
@@ -0,0 +1,11 @@
+import styles from './button.module.css'
+
+interface Button {
+    variant: string
+}
+
+const Button: FC<Button> = ({variant}) => {
+    return <div className={styles.button}>click</div>
+}
+
+export default Button
